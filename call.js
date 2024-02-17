c142a6daf66a6c37256efcb995c83b232e180434
nancy-jaiswal19 <150104311+nancy-jaiswal19@users.noreply.github.com>
2024-02-17 10:49:44 +0530

    Add files via upload

---
+++ call.js
@@ -0,0 +1,55 @@
+document.getElementById('call').addEventListener('click', function() {
+    // Replace the phone numbers with actual emergency numbers
+    var parentsNumber = '9305406903';
+    var doctorsNumber = '6387834963';
+    
+    // Call parents
+    window.open('tel:' + parentsNumber);
+    
+    // Call doctors
+    window.open('tel:' + doctorsNumber);
+});
+document.getElementById('phone1').addEventListener('click', function() {
+    // Replace the phone numbers with actual emergency numbers
+    var parentsNumber = '9305406903';
+    var doctorsNumber = '6387834963';
+    
+    // Call parents
+    window.open('tel:' + parentsNumber);
+    
+    // Call doctors
+    window.open('tel:' + doctorsNumber);
+});
+document.getElementById('phone2').addEventListener('click', function() {
+    // Replace the phone numbers with actual emergency numbers
+    var parentsNumber = '9305406903';
+    var doctorsNumber = '6387834963';
+    
+    // Call parents
+    window.open('tel:' + parentsNumber);
+    
+    // Call doctors
+    window.open('tel:' + doctorsNumber);
+});
+document.getElementById('phone3').addEventListener('click', function() {
+    // Replace the phone numbers with actual emergency numbers
+    var parentsNumber = '9305406903';
+    var doctorsNumber = '6387834963';
+    
+    // Call parents
+    window.open('tel:' + parentsNumber);
+    
+    // Call doctors
+    window.open('tel:' + doctorsNumber);
+});
+document.getElementById('phone4').addEventListener('click', function() {
+    // Replace the phone numbers with actual emergency numbers
+    var parentsNumber = '9305406903';
+    var doctorsNumber = '6387834963';
+    
+    // Call parents
+    window.open('tel:' + parentsNumber);
+    
+    // Call doctors
+    window.open('tel:' + doctorsNumber);
+});
